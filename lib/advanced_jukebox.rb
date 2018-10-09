e56a781268c61cceb0fa4adca4177a269bea7deb
Raquel Randall <raquel.sae.randall@gmail.com>
2018-10-09 19:35:23 +0000

    Done.

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -2,28 +2,33 @@
 #make sure to edit the value of each key to replace < path to this directory >
 #with the correct path to this directory on your computer
 
-# my_songs = {
-# "Go Go GO" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/01.mp3',
-# "LiberTeens" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/02.mp3',
-# "Hamburg" =>  '< path to this directory >/jukebox-cli/audio/Emerald-Park/03.mp3',
-# "Guiding Light" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/04.mp3',
-# "Wolf" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/05.mp3',
-# "Blue" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/06.mp3',
-# "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
-# }
+my_songs = {
+"Go Go GO" => 'C:/Users/Sae/.atom/.learn-ide/home/mystical-channel-3970/jukebox-cli-dumbo-web-102918/audio/Emerald-Park/01.mp3',
+"LiberTeens" => 'C:/Users/Sae/.atom/.learn-ide/home/mystical-channel-3970/jukebox-cli-dumbo-web-102918/audio/Emerald-Park/02.mp3',
+"Hamburg" =>  'C:/Users/Sae/.atom/.learn-ide/home/mystical-channel-3970/jukebox-cli-dumbo-web-102918/audio/Emerald-Park/03.mp3',
+"Guiding Light" => 'C:/Users/Sae/.atom/.learn-ide/home/mystical-channel-3970/jukebox-cli-dumbo-web-102918/audio/Emerald-Park/04.mp3',
+"Wolf" => 'C:/Users/Sae/.atom/.learn-ide/home/mystical-channel-3970/jukebox-cli-dumbo-web-102918/audio/Emerald-Park/05.mp3',
+"Blue" => 'C:/Users/Sae/.atom/.learn-ide/home/mystical-channel-3970/jukebox-cli-dumbo-web-102918/audio/Emerald-Park/06.mp3',
+"Graduation Failed" => 'C:/Users/Sae/.atom/.learn-ide/home/mystical-channel-3970/jukebox-cli-dumbo-web-102918/audio/Emerald-Park/07.mp3'
+}
 
 def help
-  #this method should be the same as in jukebox.rb
-
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
 end
 
 
 
 def list(my_songs)
-  #this method is different! Collect the keys of the my_songs hash and 
+  #this method is different! Collect the keys of the my_songs hash and
   #list the songs by name
+  my_songs.each do |song, path|
+    puts "#{song}"
+  end
 end
-
 
 def play(my_songs)
   #this method is slightly different!
@@ -33,13 +38,41 @@ def play(my_songs)
   #if it isn't, tell them their choice is invalid
   #if it is, play the song using the system 'open <file path>' syntax
   #get the file path of the song by looking it up in the my_songs hash
-  
+  puts "Please enter a song name:"
+  user_response = gets.chomp
+  my_songs.each do |song, path|
+    if user_response == song
+      puts "Playing #{song}"
+      system 'open #{path}'
+    else
+      puts "Invalid input, please try again"
+    end
+  end
 end
 
 def exit_jukebox
   #this method is the same as in jukebox.rb
+  puts "Goodbye"
 end
 
 def run(my_songs)
   #this method is the same as in jukebox.rb
+  help
+  command = ""
+  until command == "exit"
+    puts "Please enter a command:"
+    command = gets.downcase.chomp
+    case command
+    when "list"
+      list(my_songs)
+    when "play"
+      play(my_songs)
+    when "help"
+      help
+    when "exit"
+      exit_jukebox
+    else
+      help
+    end
+  end
 end
--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,52 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def list(songs)
+  songs.each.with_index(1) {|song, num| puts "#{num}. #{song}"}
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  user_response = gets.chomp
+  songs.each.with_index(1) do |song, num| # if response is a number, return index number
+    if user_response == String(num) || user_response == song
+      puts "Playing #{song}"
+    else
+      puts "Invalid input, please try again"
+    end
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  help
+  command = ""
+  until command == "exit"
+    puts "Please enter a command:"
+    command = gets.downcase.chomp
+    case command
+    when "list"
+      list(songs)
+    when "play"
+      play(songs)
+    when "help"
+      help
+    when "exit"
+      exit_jukebox
+    else
+      help
+    end
+  end
+end
